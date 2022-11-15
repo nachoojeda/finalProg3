13ada56728be371c1a91718d027d1c8548321fbd
marcoswilson1 <mwilson@udesa.edu.ar>
2022-11-15 10:13:59 -0300

    Haciendolo funcionar bien desde app

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StyleSheet, View } from 'react-native';
 
 import Register from './src/screens/Register';
+import Login from './src/screens/Login';
 
 
 const Stack = createNativeStackNavigator();
@@ -13,6 +14,7 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name='Register' component={Register} options= {{ headerShown : false}}/>
+        <Stack.Screen name='Login' component={Login} options= {{ headerShown : false}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
