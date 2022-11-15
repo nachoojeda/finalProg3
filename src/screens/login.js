3d26402fd023f389fcbcea13322412003e438120
marcoswilson1 <mwilson@udesa.edu.ar>
2022-11-15 10:10:31 -0300

    casi terminado el login, falta el estilo

--- src/screens/login.js
+++ src/screens/login.js
@@ -34,5 +34,63 @@ loguearUsuario(email,pass){
                     
 
 
+    render(){
+        return(
+            <View style={styles.container}>
+
+                
+
+
+                <Text style={styles.titulo}>Logueate</Text>
+                
+                
+                <View style={styles.form}>
+                <Text style={styles.errors}>{this.state.errors}</Text>
+
+
+                    <TextInput 
+                        placeholder= 'Email'
+                        keyboardType= 'email-address'
+                        onChangeText={ 
+                            texto => this.setState({
+                                email : texto
+                            })
+                        }
+                        value = {this.state.email}
+                        style={styles.campo}
+                    />
+                    <TextInput 
+                        placeholder= 'password'
+                        keyboardType= 'default'
+                        secureTextEntry = {true}
+                        onChangeText={ texto => this.setState({password : texto})}
+                        value = {this.state.password}
+                        style={styles.campo}
+                    />
+
+            
+
+
+            {
+                this.state.email =="" || this.state.password =="" ? 
+                    <TouchableOpacity>
+                        <Text style={styles.botonerror}>Loguearme</Text>
+                    </TouchableOpacity>
+                :
+                    <TouchableOpacity onPress={ () => this.loguearUsuario ( this.state.email, this.state.password,)}>
+                        <Text style={styles.boton}>Loguearme</Text>
+                    </TouchableOpacity>
+            }
+                    <Text onPress={ () => this.props.navigation.navigate ("Register")} style={styles.link}>¿No tenés una cuenta? Registrate</Text>
+                    
+                </View>
+            </View>
+        
+
+        )
+    }
 }
+
+
+
 export default Login;
