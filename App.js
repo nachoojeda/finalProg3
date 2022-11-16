76fd0663f5bb8408bc6330056a668a9fcf3bac58
Nacho J Ojeda Vecellio <nachojojedavecellio@Nachimac.local>
2022-11-16 15:09:48 -0300

    archivo navigation

--- App.js
+++ App.js
@@ -1,32 +1,11 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StyleSheet, View } from 'react-native';
-
-import Register from './src/screens/Register';
-import Login from './src/screens/Login';
-
-
-const Stack = createNativeStackNavigator();
+import Navigation from './src/navigation/Navigation'
 
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name='Register' component={Register} options= {{ headerShown : false}}/>
-        <Stack.Screen name='Login' component={Login} options= {{ headerShown : false}}/>
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Navigation/>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
-export default App;
+ 
+export default App; 
---
+++ src/navigation/Navigation.js
@@ -0,0 +1,34 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StyleSheet, View } from 'react-native';
+
+import Register from '../screens/Register';
+import Login from '../screens/Login';
+// import Home from '../screens/Home';
+
+
+const Stack = createNativeStackNavigator();
+
+
+function Navigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Register' component={Register} options= {{ headerShown : false}}/>
+        <Stack.Screen name='Login' component={Login} options= {{ headerShown : false}}/>
+        {/* <Stack.Screen name='Home' component={Home} options= {{ headerShown : false}}/> */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  ); 
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default Navigation;
